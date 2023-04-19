2e9140ab79291de517131a833dc3958c0e74e775
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-04-19 10:54:44 +0530

    third commit remove file

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -4,23 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.student.beans.Student;
-
 @RestController
-@RequestMapping(value="main")
+@RequestMapping(value = "main")
 public class StudentController {
-	
-	@GetMapping(value="/get")
+
+	@GetMapping(value = "/get")
 	public String getMessage() {
 		return "Welcome to the college";
-	}
-	
-	@GetMapping(value="/getstudent")
-	public Student getStudent() {
-		Student student=new Student();
-		student.setName("shahimsha");
-		student.setAge(30);
-		return student;
 	}
 
 }
